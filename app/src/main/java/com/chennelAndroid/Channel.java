d05712fad1db991611143995bfe56dc49e13eeda
Charles GBOYOU <gboyoucharles22@gmail.com>
2022-02-12 22:40:50 +0100

    Gestion interface, fichier json...

---
+++ app/src/main/java/com/chennelAndroid/Channel.java
@@ -0,0 +1,40 @@
+package com.chennelAndroid;
+
+public class Channel {
+    private String title;
+    private String chaine;
+    private String coverImage;
+
+    public Channel(){}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getChaine() {
+        return chaine;
+    }
+
+    public void setChaine(String chaine) {
+        this.chaine = chaine;
+    }
+
+    public String getCoverImage() {
+        return coverImage;
+    }
+
+    public void setCoverImage(String coverImage) {
+        this.coverImage = coverImage;
+    }
+
+    public Channel(String title, String description, String coverImage) {
+        this.title = title;
+        this.chaine = chaine;
+        this.coverImage = coverImage;
+    }
+
+}
--- app/src/main/java/com/chennelAndroid/MainActivity.java
+++ app/src/main/java/com/chennelAndroid/MainActivity.java
@@ -1,14 +1,74 @@
 package com.chennelAndroid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    List<Channel> channels;
+    private static String Json_url = "http://localhost:3000/channel";
+    Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.channelList);
+        channels = new ArrayList<>();
+        extractChannel();
+    }
+
+    private void extractChannel() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, Json_url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject songObject = response.getJSONObject(i);
+
+                        Channel channel = new Channel();
+                        channel.setTitle(songObject.getString("titre").toString());
+                        channel.setChaine(songObject.getString("chaine".toString()));
+                        channel.setCoverImage(songObject.getString("image"));
+                        channels.add(channel);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                adapter = new Adapter(getApplicationContext(),channels);
+                recyclerView.setAdapter(adapter);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("tag", "onErrorResponse: " + error.getMessage());
+            }
+        });
+
+        queue.add(jsonArrayRequest);
+
     }
 }
